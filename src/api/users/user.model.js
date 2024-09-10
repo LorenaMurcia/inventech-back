d4b317883de07e7395dc81610511d17b8780ce47
Wendy Medina <endyleyms@gmail.com>
2024-09-09 22:27:47 -0500

    modelo users y maintance

--- src/api/users/user.model.js
+++ src/api/users/user.model.js
@@ -1,11 +1,12 @@
 const { DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
 const conexion = require('../../config/db');
 
 const User = conexion.define('User', {
   username: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, allowNull: false, unique: false },
-  password: { type: DataTypes.STRING, allowNull: false }
+  password: { type: DataTypes.STRING, allowNull: false },
+  role: { type: DataTypes.STRING, allowNull: false }, // técnico, Admin, Cliente
+  status: { type: DataTypes.STRING, allowNull: false }, // activo, inactivo
 }, {
   tableName: 'users',
   timestamps: false
