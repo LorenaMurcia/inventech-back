228a0310f8c61dcb6ed40ebd09a125177b02e85a
Johan Galeano <jgaleano@ivolucion.com>
2024-09-13 09:21:39 -0500

    creo la función handleResponse para simplificar y centralizar la lógica de envío de respuestas HTTP y siempre enviar un json
    En lugar de repetir el código para enviar respuestas en cada función del controlador, handleResponse se encarga de:
    
    Establecer el código de estado HTTP: Esto indica si la solicitud fue exitosa (por ejemplo, 200, 201) o si hubo algún error (por ejemplo, 404, 500).
    Enviar la respuesta en formato JSON: Esto asegura que la respuesta siempre tenga un formato consistente.

--- src/api/tipo_dispositivo/tipo_dispositivo.controller.js
+++ src/api/tipo_dispositivo/tipo_dispositivo.controller.js
@@ -1,69 +1,66 @@
-const Tipo_dispositivo = require('./tipo_dispositivo.model')
+const TipoDispositivo = require('./tipo_dispositivo.model')
+
+const handleResponse = (res, status, data) => res.status(status).json(data);
 
 const getAllTipoDispositivo = async (req, res) => {
   try {
-    const tipo_dispositivo = await Tipo_dispositivo.findAll()
-    res.status(200).json(tipo_dispositivo)
-  }
-  catch (error) {
-    res.status(500).json({ error: 'Error al obtener los tipos de mantenimiento', error });
+    const tipoDispositivo = await TipoDispositivo.findAll()
+    handleResponse(res, 200, tipoDispositivo)
+  } catch (error) {
+    handleResponse(res, 500, { error: 'Error al obtener los tipos de mantenimiento', details: error });
   }
 }
 
 const getTipoDispositivo = async (req, res) => {
   try {
-    const { id } = req.params
-    const tipo_dispositivo = await Tipo_dispositivo.findByPk(id)
-    if (tipo_dispositivo) {
-      res.status(200).json(tipo_dispositivo)
+    const { id } = req.params;
+    const tipoDispositivo = await TipoDispositivo.findByPk(id)
+    if (tipoDispositivo) {
+      handleResponse(res, 200, tipoDispositivo)
     } else {
-      res.status(404).json({ error: 'Tipo de mantenimiento no encontrado' })
+      handleResponse(res, 404, { error: 'Tipo de mantenimiento no encontrado' })
     }
-  }
-  catch (error) {
-    res.status(500).json({ error: 'Error al obtener el tipo de mantenimiento', error });
+  } catch (error) {
+    handleResponse(res, 500, { error: 'Error al obtener el tipo de mantenimiento', details: error });
   }
 }
 
 const createTipoDispositivo = async (req, res) => {
   try {
-    const tipo_dispositivo = await Tipo_dispositivo.create(req.body)
-    res.status(201).json(tipo_dispositivo)
-  }
-  catch (error) {
-    res.status(500).json({ error: 'Error al crear el tipo de mantenimiento', error });
+    const tipoDispositivo = await TipoDispositivo.create(req.body)
+    handleResponse(res, 201, tipoDispositivo)
+  } catch (error) {
+    handleResponse(res, 500, { error: 'Error al crear el tipo de mantenimiento', details: error });
   }
 }
 
 const updateTipoDispositivo = async (req, res) => {
   try {
-    const { id } = req.params
-    const tipo_dispositivo = await Tipo_dispositivo.findByPk(id)
-    if (tipo_dispositivo) {
-      await tipo_dispositivo.update(req.body)
-      res.status(200).json(tipo_dispositivo)
+    const { id } = req.params;
+    const tipoDispositivo = await TipoDispositivo.findByPk(id)
+    if (tipoDispositivo) {
+      await tipoDispositivo.update(req.body)
+      handleResponse(res, 200, tipoDispositivo)
     } else {
-      res.status(404).json({ error: 'Tipo de mantenimiento no encontrado' })
+      handleResponse(res, 404, { error: 'Tipo de mantenimiento no encontrado' })
     }
-  }
-  catch (error) {
-    res.status(500).json({ error: 'Error al actualizar el tipo de mantenimiento', error });
+  } catch (error) {
+    handleResponse(res, 500, { error: 'Error al actualizar el tipo de mantenimiento', details: error });
   }
 }
 
 const deleteTipoDispositivo = async (req, res) => {
   try {
     const { id } = req.params
-    const tipo_dispositivo = await Tipo_dispositivo.findByPk(id)
-    if (tipo_dispositivo) {
-      await tipo_dispositivo.destroy()
-      res.status(200).json({ message: 'Tipo de mantenimiento eliminado correctamente' })
+    const tipoDispositivo = await TipoDispositivo.findByPk(id)
+    if (tipoDispositivo) {
+      await tipoDispositivo.destroy()
+      handleResponse(res, 200, { message: 'Tipo de mantenimiento eliminado correctamente' })
     } else {
-      res.status(404).json({ error: 'Tipo de mantenimiento no encontrado' })
+      handleResponse(res, 404, { error: 'Tipo de mantenimiento no encontrado' })
     }
-  }
-  catch (error) {
-    res.status(500).json({ error: 'Error al eliminar el tipo de mantenimiento', error });
+  } catch (error) {
+    handleResponse(res, 500, { error: 'Error al eliminar el tipo de mantenimiento', details: error })
   }
 }
 
