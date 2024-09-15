74cfa01164e58ee15dda9e35ec0004531f2780a0
Johan Galeano <jgaleano@ivolucion.com>
2024-09-15 17:00:25 -0500

    Update traza_mantenimiento.model.js

--- src/api/traza_mantenimiento/traza_mantenimiento.model.js
+++ src/api/traza_mantenimiento/traza_mantenimiento.model.js
@@ -5,8 +5,9 @@ const Equipos = require('../equipos/equipos.model');
 const Mantenimientos = require('../mantenimientos/mantenimientos.model');
 
 const Traza_mantenimiento = conexion.define('Traza_mantenimiento', {
+
     id_traza: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    fecha: { type: DataTypes.DATE, allowNull: false },
+    fecha:{ type: DataTypes.DATE, allowNull: false },
     descripcion: { type: DataTypes.STRING, allowNull: false },
     id_estado: { type: DataTypes.INTEGER, allowNull: false ,
         references:{
@@ -24,22 +25,7 @@ const Traza_mantenimiento = conexion.define('Traza_mantenimiento', {
     }}
 }, {
     tableName: 'traza_mantenimiento',
-    timestamps: false,
-});
-
-Traza_mantenimiento.belongsTo(Estado_equipos, {
-    foreignKey: 'id_estado',
-    as: 'estado'
-});
-
-Traza_mantenimiento.belongsTo(Equipos, {
-    foreignKey: 'serial',
-    as: 'equipo'
-});
-
-Traza_mantenimiento.belongsTo(Mantenimientos, {
-    foreignKey: 'id_mantenimiento',
-    as: 'mantenimiento'
+        timestamps: false,
 });
 
 Traza_mantenimiento.belongsTo(Estado_equipos,{ 
