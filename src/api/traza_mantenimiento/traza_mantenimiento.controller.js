a89004a514804850b8ac14fc928f739e72d5f0e1
Johan Galeano <jgaleano@ivolucion.com>
2024-09-17 14:48:56 -0500

    creo y edito la traza de mantenimientos

--- src/api/traza_mantenimiento/traza_mantenimiento.controller.js
+++ src/api/traza_mantenimiento/traza_mantenimiento.controller.js
@@ -8,34 +8,34 @@ const createTraza = async (req, res) => {
         const { fecha, descripcion, id_estado, serial, id_mantenimiento } = req.body;
         if (!fecha || !descripcion || !id_estado || !serial || !id_mantenimiento) {
             return res.status(400).json({ message: 'Todos los datos son necesarios' });
-
         }
+
         const mantenimientos = await Mantenimientos.findOne({
             where: {
                 id_mantenimiento: id_mantenimiento
             }
-        });        
+        });
 
-        if(!mantenimientos){
-            return res.status(401).json({message: 'El id del mantenimiento no existe'})
-        } 
-
-        const equipos = await Equipos.findOne({
-            where:{
-                serial:serial
-            }
-        })
-
-        if(!equipos){
-            return res.status(401).json({message: 'El serial del equipo no existe'})
+        if (!mantenimientos) {
+            return res.status(401).json({ message: 'El id del mantenimiento no existe' });
         }
 
-        const traza = await Traza.create(body);
+        const equipos = await Equipos.findOne({
+            where: {
+                serial: serial
+            }
+        });
+
+        if (!equipos) {
+            return res.status(401).json({ message: 'El serial del equipo no existe' });
+        }
+
+        const traza = await Traza.create(req.body);
         res.status(200).json(traza);
     } catch (error) {
-        res.status(500).json({ message: 'Error al crear la traza' + error })
+        res.status(500).json({ message: 'Error al crear la traza: ' + error });
     }
-}
+};
 
 const getAllTraza = async (req, res) => {
     try {
@@ -66,4 +66,18 @@ const getOneTraza = async (req, res) => {
     }
 }
 
-module.exports = { createTraza, getAllTraza, getOneTraza }
+const updateTraza = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const traza = await Traza.findByPk(id);
+        if (!traza) {
+            return res.status(404).json({ message: 'Traza no encontrada' })
+        }
+        await traza.update(req.body);
+        res.status(200).json({ message: 'Traza actualizada' })
+    } catch (error) {
+        res.status(500).json({ message: 'Error al actualizar la traza' + error })
+    }
+}
+
+module.exports = { createTraza, getAllTraza, getOneTraza, updateTraza }
